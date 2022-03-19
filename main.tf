d196aa74d5dfc59ad335076230e6ed3921fb4560
Igor Sodré <csodreigor@gmail.com>
2022-03-19 11:34:23 -0300

    aded main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "tf_test" {
+  location = "Brazil South"
+  name     = "tfmainrg"
+}
+
+resource "azurerm_container_group" "tfcg_test" {
+  location            = azurerm_resource_group.tf_test.location
+  name                = "terraform-practice-api"
+  resource_group_name = azurerm_resource_group.tf_test.name
+  os_type             = "Linux"
+  ip_address_type     = "public"
+  dns_name_label      = "igorsodrewa"
+  
+  container {
+    cpu    = 1
+    image  = "igorsodre/terraform-practice"
+    memory = 1
+    name   = "terraform-practice-api"
+    ports {
+      port     = 80
+      protocol = "TCP"
+    }
+  }
+}
