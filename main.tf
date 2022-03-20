88b9661d8438022bcb253ee19752d4eb3088573e
Igor Sodré <csodreigor@gmail.com>
2022-03-19 23:24:52 -0300

    add build version to pipeline

--- main.tf
+++ main.tf
@@ -4,11 +4,16 @@ provider "azurerm" {
 
 terraform {
   backend "azurerm" {
-    resource_group_name = "tf_rs_blobstore"
+    resource_group_name  = "tf_rs_blobstore"
     storage_account_name = "tfstorageigorsodre"
-    container_name = "tfstate"
-    key = "terraform.tfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
   }
+}
+
+variable "IMAGEBUILD" {
+  type        = string
+  description = "latest image build"
 }
 
 resource "azurerm_resource_group" "tf_test" {
@@ -23,10 +28,10 @@ resource "azurerm_container_group" "tfcg_test" {
   os_type             = "Linux"
   ip_address_type     = "public"
   dns_name_label      = "igorsodrewa"
-  
+
   container {
     cpu    = 1
-    image  = "igorsodre/terraform-practice"
+    image  = "igorsodre/terraform-practice:${var.IMAGEBUILD}"
     memory = 1
     name   = "terraform-practice-api"
     ports {
